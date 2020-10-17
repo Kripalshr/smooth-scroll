d26567021079e9f91adf71923559950b17563cf8
kripalshr <kripalshr@gmail.com>
2020-10-17 21:06:10 +0545

    Finished Project for practice

---
+++ src/pages/index.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import Navbar from '../components/Navbar/index';
+import Sidebar from '../components/Sidebar/index';
+import HeroSection from '../components/HeroSection/index';
+import InfoSection from '../components/InfoSection/index';
+import { 
+    homeObjOne, 
+    homeObjTwo, 
+    homeObjThree,
+} from '../components/InfoSection/Data';
+import Services from '../components/Services/index';
+import Footer from '../components/Footer/index';
+
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />  
+            <HeroSection />
+            <InfoSection {...homeObjOne} />
+            <InfoSection {...homeObjTwo} />
+            <Services />
+            <InfoSection {...homeObjThree} />
+            <Footer />
+        </>
+    );
+};
+
+export default Home
